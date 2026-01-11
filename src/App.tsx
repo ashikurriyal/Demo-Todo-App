ba17bf549088ce735a853ec26ec57f1ffb0f66f6
ashikurriyal <riyal.ashikur@gmail.com>
2026-01-11 07:20:23 +0600

    initializing shadcn to the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
-import './App.css'
+import "./App.css";
+import { Button } from "./components/ui/button";
 
 function App() {
-
   return (
     <>
-      <h1 className='font-bold text-3xl '>To-Do APP</h1>
+      <h1 className="font-bold text-3xl ">Basic Todo App</h1>
+      <Button>Test Button</Button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
